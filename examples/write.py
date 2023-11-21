8dc686391485a841ace6c131609e2453d34286f5
williamfzc <178894043@qq.com>
2023-11-22 00:39:26 +0800

    chore: test

--- examples/write.py
+++ examples/write.py
@@ -1,8 +1,9 @@
 from srctag.collector import FileLevelEnum, Collector
 from srctag.storage import Storage
+import os
 
 collector = Collector()
-collector.config.repo_root = ".."
+collector.config.repo_root = os.path.dirname(os.path.dirname(__file__))
 collector.config.file_level = FileLevelEnum.DIR
 collector.config.max_depth_limit = 16
 
