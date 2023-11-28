2f9cc4eb2ae7f08d5929e1f769631befdd530e28
williamfzc <178894043@qq.com>
2023-11-28 20:55:25 +0800

    feat: cli for starting analyze

--- srctag/cli.py
+++ srctag/cli.py
@@ -4,6 +4,10 @@ import click
 from chromadb.utils.embedding_functions import SentenceTransformerEmbeddingFunction
 from loguru import logger
 
+from srctag.collector import Collector, FileLevelEnum
+from srctag.storage import Storage
+from srctag.tagger import Tagger
+
 
 @click.group()
 def cli():
@@ -27,6 +31,40 @@ def prepare():
     )
     assert collection.count() == 1
     click.echo("ok.")
+
+
+@cli.command()
+@click.option("--repo-root", default=".", help="Repository root directory")
+@click.option("--max-depth-limit", default=-1, help="Maximum depth limit")
+@click.option("--include-regex", default="", help="File include regex pattern")
+@click.option("--tags-file", type=click.File("r"), default="./srctag.txt", help="Path to a text file containing tags")
+@click.option("--output-path", default="", help="Output file path for CSV")
+@click.option("--file-level", default=FileLevelEnum.FILE, help="Scan file level, FILE or DIR, default to FILE")
+@click.option("--st-model", default="", help="Sentence Transformer Model")
+def tag(repo_root, max_depth_limit, include_regex, tags_file, output_path, file_level, st_model):
+    """ tag your repo """
+    collector = Collector()
+    collector.config.repo_root = repo_root
+    collector.config.max_depth_limit = max_depth_limit
+    collector.config.include_regex = include_regex
+    collector.config.file_level = file_level
+
+    ctx = collector.collect_metadata()
+    storage = Storage()
+    storage.config.st_model_name = st_model
+    storage.embed_ctx(ctx)
+    tagger = Tagger()
+
+    assert tags_file, "no tag file provided"
+    tags = [each.strip() for each in tags_file.read().splitlines()]
+
+    tagger.config.tags = tags
+    tag_dict = tagger.tag(storage)
+
+    if output_path:
+        tag_dict.export_csv(path=output_path)
+    else:
+        tag_dict.export_csv()
 
 
 if __name__ == '__main__':
