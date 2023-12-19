35fb25bf3393d7c28a7608ee69ccaf3262022d85
williamfzc <178894043@qq.com>
2023-12-20 00:15:33 +0800

    chore: case

--- tests/test_collector.py
+++ tests/test_collector.py
@@ -1,5 +1,8 @@
 import os
 
+import networkx as nx
+from matplotlib import pyplot as plt
+
 from srctag.collector import Collector
 
 
@@ -9,3 +12,16 @@ def test_tagger_specific():
     collector.config.include_file_list = ["README.md"]
     ctx = collector.collect_metadata()
     assert len(ctx.files) == 1
+
+
+def test_relations():
+    collector = Collector()
+    collector.config.repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    ctx = collector.collect_metadata()
+    relations = ctx.relations
+    assert relations
+    assert relations.nodes
+    assert relations.edges
+    nx.draw(relations, with_labels=True, font_weight='bold', node_size=400,
+            font_color='black', font_size=4, edge_color='gray', alpha=0.7)
+    plt.savefig("my_graph.svg")
